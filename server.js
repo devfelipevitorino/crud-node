53aa014fd447a224d614139fef5b0db004317957
devfelipevitorino <devfelipevitorino@gmail.com>
2025-04-15 20:47:23 -0300

    get e post

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
 app.listen(3000, function(){
     console.log('server running on port 3000')
 })
 
+app.use(bodyParser.urlencoded({ extended: true}))
+
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
+})
+
+app.post('/formulario', (req, res) => {
+    console.log(req.body)
 })
