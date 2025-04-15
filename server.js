a7ee2ec1fe3d8202c2c5de6083927026ec637b03
devfelipevitorino <devfelipevitorino@gmail.com>
2025-04-15 20:39:49 -0300

    hello world nodejs

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.listen(3000, function(){
+    console.log('server running on port 3000')
+})
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
