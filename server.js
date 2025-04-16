f0af0c3a5104245379f17e79422334665ac9d7e8
devfelipevitorino <devfelipevitorino@gmail.com>
2025-04-15 22:31:54 -0300

    post salvando no mongodb

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
+const app = express();
 
-app.listen(3000, function(){
-    console.log('server running on port 3000')
-})
+const uri = "mongodb+srv://felipe:teste@cluster0.qv2cgic.mongodb.net/?retryWrites=true&w=majority&appName=bancoCRUD";
 
-app.use(bodyParser.urlencoded({ extended: true}))
+MongoClient.connect(uri, { useUnifiedTopology: true })
+    .then(client => {
+        console.log('Conectado ao MongoDB Atlas');
+        const db = client.db('bancoCRUD');
+        const collection = db.collection('usuarios');
 
-app.set('view engine', 'ejs')
+        app.use(bodyParser.urlencoded({ extended: true }));
+        app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render('index.ejs')
-})
+        app.get('/', (req, res) => {
+            res.render('index.ejs');
+        });
 
-app.post('/formulario', (req, res) => {
-    console.log(req.body)
-})
+        app.post('/formulario', (req, res) => {
+            collection.insertOne(req.body)
+                .then(result => {
+                    console.log('Documento salvo com sucesso!');
+                    res.redirect('/');
+                })
+                .catch(err => console.error('Erro ao salvar documento:', err));
+        });
+
+        app.listen(3000, () => {
+            console.log('Servidor rodando na porta 3000');
+        });
+    })
+    .catch(err => console.error('Erro ao conectar ao MongoDB:', err));
